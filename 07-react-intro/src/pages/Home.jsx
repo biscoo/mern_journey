15445097790029242be743fc95cfab5016407d10
Beshoy Beshara <eng.beshoy.91@outlook.com>
2026-06-19 17:24:26 +0200

    Connect Home page to Notes API

--- 07-react-intro/src/pages/Home.jsx
+++ 07-react-intro/src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    // Simulate fetching notes from an API
+    async function loadNotes() {
+      const response = await fetch("http://localhost:3000/notes");
+      const data = await response.json();
+      setNotes(data);
+    }
+
+    loadNotes();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      <ul>
+        {notes.map((note) => {
+          return <li key={note._id}>{note.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
